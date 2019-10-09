f46cf3dbcb2c7ae6f2cdbbc49ab97957344a21bd
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2019-10-09 12:24:37 -0400

    Use 'uniqid' instead of 'rand' to avoid collisions

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        'code' => "CODE" . rand(10000,99999),
+        'code' => uniqid("CODE"),
         'name' => $faker->word(2),
         'description' => $faker->paragraph(2),
         'price' => $faker->numberBetween(50, 50000 ), // Integer cents
