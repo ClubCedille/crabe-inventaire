ac9594a950e0898bfe5329500b59a5d4b7bd9ef6
Roch D'Amour <roch.damour@gmail.com>
2020-02-19 21:21:50 -0500

    Oublie pas tes accents

--- resources/lang/fr/product.php
+++ resources/lang/fr/product.php
@@ -13,6 +13,6 @@ return [
     'quantity' => 'Quantite',
     'category' => 'Categorie',
     'price' => 'Prix',
-    'updated' => 'Le produit a ete mis a jour',
-    'created' => 'Le produit a ete cree',
+    'updated' => 'Le produit a été mis a jour',
+    'created' => 'Le produit a été cree',
 ];
